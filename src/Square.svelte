ebaa368005262ceb7e168b02618331221c5e0d50
Dan Simpson <daniel.simpson@alintaenergy.com.au>
2020-02-14 11:15:27 +1100

    copy from REPL

---
+++ src/Square.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { fade } from "svelte/transition";
+
+  export let x;
+  export let y;
+
+  $: isEven = (x + y) % 2 === 0;
+</script>
+
+<style>
+  div {
+    min-width: 50px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-items: center;
+    border: 1px solid black;
+  }
+
+  .even {
+    background-color: rgba(100, 100, 100, 0.5);
+  }
+
+  p {
+    display: block;
+    margin: 0 auto;
+    text-align: center;
+  }
+</style>
+
+<div transition:fade={{ duration: 100 }} class:even={isEven}>
+  <p>{y + 1}</p>
+</div>
---
+++ src/SquareGrid.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Square from "./Square.svelte";
+  export let size;
+
+  $: enumerator = new Array(size);
+</script>
+
+<style>
+  div.grid {
+    display: flex;
+    flex-direction: column;
+  }
+
+  div.row {
+    display: flex;
+    flex-direction: row;
+  }
+</style>
+
+<p>Number of squares: {size * size}</p>
+
+<div class="grid">
+  {#each enumerator as _, x}
+    <div class="row">
+      {#each enumerator as _, y}
+        <Square {x} {y}>x:{x} y:{y}</Square>
+      {/each}
+    </div>
+  {/each}
+</div>
